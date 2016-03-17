65e21b138d298c695f59aada0e5ae82dce008e90
Michele Colombo <mikexine@gmail.com>
2016-03-17 17:07:08 +0100

    fix bug

--- SendToSubscribers.py
+++ SendToSubscribers.py
@@ -13,9 +13,8 @@ config = ConfigParser.ConfigParser()
 config.read('settings.ini')
 token = config.get('main', 'token')
 
-mymessage = "*ELEZIONI STUDENTESCHE 2016*\n\nIl *18 e il 19 maggio* potrai scegliere i tuoi nuovi rappresentanti nei Consigli di Corso, negli Organi Maggiori e nel Consiglio Nazionale degli Studenti Universitari.\n\nCandidati con *UDU - Studenti Per*! Il termine ultimo per le candidature è il *5 aprile*. Sei interessato, ma hai dubbi o perplessità? Puoi contattare @francocorti92 o @alejo91 :)"
+mymessage = "*ELEZIONI STUDENTESCHE 2016*\n\n*Il 18 e il 19 maggio* potrai scegliere i tuoi nuovi rappresentanti nei Consigli di Corso, negli Organi Maggiori e nel Consiglio Nazionale degli Studenti Universitari. Il termine ultimo per le candidature è il *5 aprile*.\n\nSe hai idee per migliorare la tua vita e quella di altri studenti o se anche solo vuoi conoscere meglio la rappresentanza studentesca le porte di *UDU - Studenti Per* sono sempre aperte!\n\nSe sei interessato, ma hai dubbi o perplessità puoi contattare @francocorti92 o @alejo91 :)"
 text = mymessage
-
 print 'sending message: ' + text
 
 con = sqlite3.connect("db/logs.db")
