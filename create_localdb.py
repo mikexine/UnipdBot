5e1957ae47ee55e1a90522e1a2a3dc2035ee811b
Michele Colombo <mikexine@gmail.com>
2016-05-11 05:38:41 +0200

    add posti pinali

--- create_localdb.py
+++ create_localdb.py
@@ -87,7 +87,7 @@ sleep(2)
 
 biblioteca = requests.get(URL + 'biblioteca/', headers=HEADERS).json()
 for key in biblioteca:
-    if key == "metelli":
+    if key == "metelli" or key == "pinali":
         text = "*%s*\nPosti liberi: %s\nIndirizzo: %s\n_Orari:_ %s.\n" % \
            (biblioteca[key]['nome'].encode("utf-8"),
             biblioteca[key]['posti'].encode("utf-8"),
